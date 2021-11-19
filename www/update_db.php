91d4a7305ef3676e847befa186313a3630e023c4
Evgenii Sopov <mrseakg@gmail.com>
2021-11-19 23:27:39 +0700

    Fixed www/update_db.php

--- www/update_db.php
+++ www/update_db.php
@@ -7,7 +7,7 @@ $conn = BaseLib::dbConn();
 
 $current_db_ver = 0;
 
-$stmt = $conn->prepare('SELECT * FROM webdiff_db_updates ORDER BY dt DESC LIMIT 0,1');
+$stmt = $conn->prepare('SELECT * FROM webdiff_db_updates ORDER BY dt DESC, version DESC LIMIT 0,1');
 $stmt->execute(array());
 
 if ($row = $stmt->fetch()) {
@@ -20,6 +20,7 @@ function next_func($cv) {
   return 'update'.str_pad("".($cv+1), 4, "0", STR_PAD_LEFT);
 }
 
+echo "Current db version: ".$current_db_ver."<br>";
 $funcname = next_func($current_db_ver);
 $fileupdate = $funcname.'.php';
 
@@ -27,13 +28,19 @@ while (file_exists($curdir_update_db.'/db_updates/'.$fileupdate)) {
   include_once($curdir_update_db.'/db_updates/'.$fileupdate);
   if (function_exists($funcname)) {
     $response['apply_updates'] = $funcname;
+    echo "Applying ".$funcname."<br>";
     if ($funcname($conn)) {
-      $stmt = $conn->prepare('INSERT INTO webdiff_db_updates(version,dt) VALUES(?,NOW())');
+      $stmt = $conn->prepare('INSERT INTO webdiff_db_updates(`version`,dt) VALUES(?,NOW())');
       $stmt->execute(array($current_db_ver+1));
       $response['current_db_ver'] = $current_db_ver+1;
+      echo "Done<br>";
     } else {
-      StaticLib::error(500, "Failed update");
+      echo "Failed execute update ".$funcname;
+      exit;
     }
+  } else {
+    echo "Failed run function ".$funcname;
+    exit;
   }
   // next update
   $current_db_ver++;
