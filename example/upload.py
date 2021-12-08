0bbf27fd65a54b90ee59c48ae2165d3147898fd4
Evgenii Sopov <mrseakg@gmail.com>
2021-12-08 10:45:48 +0700

    Redesign format of upload.py

--- example/upload.py
+++ example/upload.py
@@ -7,6 +7,7 @@ import sys
 import re
 import stat
 import requests
+import datetime
 
 # BASEURL = "http://localhost:8071/"
 BASEURL = "http://localhost:1234"
@@ -28,6 +29,37 @@ def remove_first_folder(_file):
     _file = _file[1:]
     return "/".join(_file)
 
+def upload_files(data_request, _uploaded, _all):
+    print(" ------- ")
+    print("Start adding information. Group: " + data_request['group'] + "; Version: " + data_request['version'])
+    print("Uploading " + str(len(data_request['files'])) + "....")
+    r = requests.post(BASEURL + '/api/add/', json = data_request)
+    if r.status_code != 200:
+        sys.exit("Could not post " + str(r.status_code ) + r.content)
+    _uploaded += len(data_request['files'])
+    print("Uploaded " + str(_uploaded) + "/" + str(_all))
+    return _uploaded
+
+def getfileinfo(_zip, _reg_file):
+    ret = {}
+    fi = _zip.getinfo(_reg_file)
+    ret["name"] = remove_first_folder(_reg_file)
+    ret["size"] = fi.file_size
+    ret["mode"] = stat.filemode(fi.external_attr >> 16)
+    ret["compress_size"] = fi.compress_size
+    ret["is_dir"] = fi.is_dir()
+    dt = datetime.datetime(
+        fi.date_time[0],
+        fi.date_time[1],
+        fi.date_time[2],
+        fi.date_time[3],
+        fi.date_time[4],
+        fi.date_time[5]
+    )
+    ret["dt"] = dt.strftime("%Y-%m-%dT%H:%M:%SZ")
+    print(ret)
+    return ret
+
 files = os.listdir(".")
 for _zip_filename in files:
     if not _zip_filename.endswith(".zip"):
@@ -38,47 +70,37 @@ for _zip_filename in files:
     data_request['version'] = version
     data_request['files'] = []
 
-    zip = zipfile.ZipFile(_zip_filename)
-    reg_files = zip.namelist()
+    _zip = zipfile.ZipFile(_zip_filename)
+    reg_files = _zip.namelist()
     # print(reg_files)
-    for f in reg_files:
-        # https://docs.python.org/3/library/zipfile.html#zipinfo-objects
-        fi = zip.getinfo(f)
-        filemode = stat.filemode(fi.external_attr >> 16)
-        print(
-            f + " size: "
-            + str(fi.file_size)
-            + ", compress_size: "
-            + str(fi.compress_size)
-            + ", is_dir: "
-            + str(fi.is_dir())
-            + ", date_time: "
-            + str(fi.date_time)
-            + ", filemode: "
-            + str(filemode)
-        )
+    # for f in reg_files:
+    #     # https://docs.python.org/3/library/zipfile.html#zipinfo-objects
+    #     fi = _zip.getinfo(f)
+    #     filemode = stat.filemode(fi.external_attr >> 16)
+    #     print(
+    #         f + " size: "
+    #         + str(fi.file_size)
+    #         + ", compress_size: "
+    #         + str(fi.compress_size)
+    #         + ", is_dir: "
+    #         + str(fi.is_dir())
+    #         + ", date_time: "
+    #         + str(fi.date_time)
+    #         + ", filemode: "
+    #         + str(filemode)
+    #     )
 
     _all = len(reg_files)
     _uploaded = 0
     print(" ------- ")
     print("Start adding information. Group: " + data_request['group'] + "; Version: " + data_request['version'])
     for _reg_file in reg_files:
-        _reg_file = remove_first_folder(_reg_file)
-        data_request['files'].append(_reg_file)
+        data_request['files'].append(getfileinfo(_zip, _reg_file))
         if len(data_request['files']) > 499:
-            print("Uploading " + str(len(data_request['files'])) + "....")
-            r = requests.post(BASEURL + '/api/add/', json = data_request)
-            if r.status_code != 200:
-                sys.exit("Could not post " + str(r.status_code ) + r.content)
-            _uploaded += len(data_request['files'])
-            print("Uploaded " + str(_uploaded) + "/" + str(_all))
+            _uploaded = upload_files(data_request, _uploaded, _all)
             data_request['files'] = []
     if len(data_request['files']) > 0:
-        r = requests.post(BASEURL + '/api/add/', json = data_request)
-        if r.status_code != 200:
-            sys.exit("Could not post " + str(r.status_code ) + str(r.content))
-        str(r.content)
+        _uploaded = upload_files(data_request, _uploaded, _all)
         _uploaded += len(data_request['files'])
-        print("Uploaded " + str(_uploaded) + "/" + str(_all))
         data_request['files'] = []
     print("Done.")
